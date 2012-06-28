2643bafc75e86516bce8b6a2ca3132ee79c06ca5
Max Stoliar <hexelon@gmail.com>
2012-06-28 10:14:09 +0300

    changed float numbers work

--- limestone.js
+++ limestone.js
@@ -734,7 +734,7 @@ exports.SphinxClient = function() {
 
                 // FLOAT size attributes (32 bits)
                 if (output.attributes[attribute].type == Sphinx.attribute.FLOAT) {
-                    attr_value = response.int32();
+                    attr_value = response.float();
                     match.attrs[output.attributes[attribute].name] = attr_value;
                     continue;
                 }
